4e88ae26cb16ae390e0559d900b099e415884759
samuel <samuel@samuel-VirtualBox.(none)>
2013-02-21 23:08:07 +0000

    Minor bug fix. Remvoed checks.

--- player-stage-install.sh
+++ player-stage-install.sh
@@ -25,7 +25,7 @@ wget https://github.com/rtv/Stage/archive/v$stage_version.tar.gz
 #extract them both
 echo "Extracting..."
 tar xvf player-$player_version.tar.gz
-tar xvf $stage_version.tar.gz
+tar xvf v$stage_version.tar.gz
 
 #Patching Player
 echo "$patch_file" > ~/src/player-$player_version/server/drivers/shell/readlog.cc
@@ -37,12 +37,6 @@ mkdir build
 cd build
 cmake ..
 sudo make install
-
-if [ $? -eq 0 ]
-	then
-	echo "Failed to build player. Quitting."
-	exit
-fi
 
 #switch to Stage directory
 cd ../..
@@ -59,10 +53,3 @@ cd build
 cmake ..
 sudo make install
 
-if [ $? -eq 0 ]
-	then
-	echo "Failed to build stage. Quitting."
-	exit
-fi
-
-echo "Finished successfully!"
